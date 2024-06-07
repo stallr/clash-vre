a93bf29a1edff0c21fb52dd03cefb989905a20dc
stallr <46423822+stallr@users.noreply.github.com>
2024-06-08 04:09:21 +0900

    fix linux

--- src-tauri/src/core/manager.rs
+++ src-tauri/src/core/manager.rs
@@ -10,7 +10,7 @@ pub fn grant_permission(core: String) -> anyhow::Result<()> {
     log::debug!("grant_permission path: {path}");
 
     #[cfg(target_os = "macos")]
-    if getcore_path(&path) {
+    if !getcore_path(&path) {
         Ok(())
     }else{
         let output = {
@@ -30,30 +30,32 @@ pub fn grant_permission(core: String) -> anyhow::Result<()> {
     }
 
     #[cfg(target_os = "linux")]
-    let output = {
-        let path = path.replace(' ', "\\ "); // 避免路径中有空格
-        let shell = format!("setcap cap_net_bind_service,cap_net_admin,cap_dac_override=+ep {path}");
-
-        let sudo = match Command::new("which").arg("pkexec").output() {
-            Ok(output) => {
-                if output.stdout.is_empty() {
-                    "sudo"
-                } else {
-                    "pkexec"
-                }
-            }
-            Err(_) => "sudo",
-        };
-
-        Command::new(sudo).arg("sh").arg("-c").arg(shell).output()?
-    };
-
-    #[cfg(target_os = "linux")]
-    if output.status.success() {
+    if !getcore_path(&path) {
         Ok(())
-    } else {
-        let stderr = std::str::from_utf8(&output.stderr).unwrap_or("");
-        anyhow::bail!("{stderr}");
+    }else{
+        let output = {
+            let path = path.replace(' ', "\\ "); // 避免路径中有空格
+            let shell = format!("setcap cap_net_bind_service,cap_net_admin,cap_dac_override=+ep {path}");
+
+            let sudo = match Command::new("which").arg("pkexec").output() {
+                Ok(output) => {
+                    if output.stdout.is_empty() {
+                        "sudo"
+                    } else {
+                        "pkexec"
+                    }
+                }
+                Err(_) => "sudo",
+            };
+
+            Command::new(sudo).arg("sh").arg("-c").arg(shell).output()?
+        };
+        if output.status.success() {
+            Ok(())
+        } else {
+            let stderr = std::str::from_utf8(&output.stderr).unwrap_or("");
+            anyhow::bail!("{stderr}");
+        }
     }
 }
 
@@ -69,6 +71,24 @@ pub fn getcore_path(path: &str) -> bool {
             let is_setuid_set = permissions & 0o4000 != 0;
             let is_setgid_set = permissions & 0o2000 != 0;
             is_owner_root && is_group_admin && is_setuid_set && is_setgid_set
+        }
+        Err(_) => false,
+    }
+}
+
+#[cfg(target_os = "linux")]
+pub fn getcore_path(path: &str) -> bool {
+    use std::fs;
+    use std::os::unix::fs::MetadataExt;
+    
+    match fs::metadata(path) {
+        Ok(metadata) => {
+            let is_owner_root = metadata.uid() == 0;
+            let is_group_root = metadata.gid() == 0; // 在很多Linux系统上，root组的GID为0
+            let permissions = metadata.mode();
+            let is_setuid_set = permissions & 0o4000 != 0;
+            let is_setgid_set = permissions & 0o2000 != 0;
+            is_owner_root && is_group_root && is_setuid_set && is_setgid_set
         }
         Err(_) => false,
     }
